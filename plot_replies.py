cb6f2770bcdc590fa9e1ccf833d6f5e851ed7fe3
Michaël Trazzi <michael.trazzi@gmail.com>
2020-12-06 16:00:45 +0100

    oh no ; that's ugly let's commit more

--- plot_replies.py
+++ plot_replies.py
@@ -1,5 +1,5 @@
-import pandas as pd;
-import matplotlib.pyplot as plt;\
+import pandas as pd
+import matplotlib.pyplot as plt
 
 # read your reply data into pandas dataframe
 df = pd.read_csv('data.csv') 
@@ -9,9 +9,9 @@ reply_gang = df['Screen name']
 
 # plot the names that appear the most often
 plt.rcParams["figure.figsize"] = (10,5)
-reply_gang.value_counts()[:15].sort_values().plot(kind = 'barh');
+reply_gang.value_counts()[:15].sort_values().plot(kind = 'barh')
 
 # show the plots
-plt.title('# replies');
+plt.title('# replies')
 plt.savefig('replies.png')
 plt.show()
